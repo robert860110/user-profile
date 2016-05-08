af79d5234079fe970b406454879d82eff5db13f3
Guanqun Bao <guanqun.bao@gmail.com>
2016-05-08 14:57:26 -0700

    initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var PORT = 3000;
+var express = require('express');
+var app = express();
+
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log('pricate route hit');
+        next();
+    },
+    logger: function(req, res, next) {
+        console.log(req.method + new Date().toString());
+        next();
+    }
+
+};
+
+
+
+app.use(middleware.logger);
+
+app.get('/home', middleware.requireAuthentication, function(req, res) {
+    res.send('hello express');
+
+})
+
+app.use(express.static(__dirname + '/public'));
+
+//console.log(__dirname);
+
+app.listen(PORT, function() {
+    console.log('The server has started on port:' + ' ' + PORT);
+});
